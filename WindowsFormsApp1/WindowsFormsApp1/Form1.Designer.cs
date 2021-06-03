c32ae5ac24bfbb97b197f9a31e1df31e6eba8397
imadiz <weszerle42@gmail.com>
2021-06-03 09:08:03 +0200

    gomb 2

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Gomb 2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
